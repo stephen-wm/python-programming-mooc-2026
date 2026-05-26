ed7ab82288376eb371536c4c49e5c1a7888b7b49
Stephen <hellostephenwm@gmail.com>
2026-05-26 12:38:10 -0400

    chore: add exercise 32

---
+++ part04/part04-32_integers_to_strings.py
@@ -0,0 +1,12 @@
+def formatted(my_list: list):
+    new_list = []
+
+    for item in my_list:
+        new_list.append(f"{item:.2f}")
+    
+    return new_list
+
+if __name__ == "__main__":
+    my_list = [1.234, 0.3333, 0.11111, 3.446]
+    new_list = formatted(my_list)
+    print(new_list)
